41fa83f6ee04b6af119854ae53b454287347b45d
RA341 <razerishi1505@gmail.com>
2025-01-29 04:19:15 -0500

    added lobby stream reconnect

--- frontend/lib/clients/lobby_api.dart
+++ frontend/lib/clients/lobby_api.dart
@@ -8,11 +8,36 @@ final lobbyApiProvider = Provider<LobbyServiceClient>((ref) {
   return LobbyServiceClient(channel);
 });
 
-final lobbyListProvider = StreamProvider.autoDispose<List<Lobby>>((ref) async* {
-  final grpcLobbies =
-      ref.watch(lobbyApiProvider).listLobbies(ListLobbiesRequest());
+final reconnectProvider = StateProvider<Duration>((ref) {
+  return Duration.zero;
+});
 
-  await for (final lobbyList in grpcLobbies) {
-    yield lobbyList.lobbies;
+final lobbyListProvider = StreamProvider.autoDispose<List<Lobby>>((ref) async* {
+  final lobbyApi = ref.watch(lobbyApiProvider);
+  int attempt = 0;
+
+  while (true) {
+    try {
+      final grpcLobbies = lobbyApi.listLobbies(ListLobbiesRequest());
+
+      await for (final lobbyList in grpcLobbies) {
+        ref.read(reconnectProvider.notifier).state = Duration.zero;
+        attempt = 0; // Reset attempt on success
+        yield lobbyList.lobbies;
+      }
+    } catch (e) {
+      attempt++;
+      final delay =
+          Duration(milliseconds: 500 * (1 << attempt)); // Exponential backoff
+
+      ref.read(reconnectProvider.notifier).state = delay;
+      await Future.delayed(delay);
+
+      if (attempt > 10) {
+        // reset duration, on error, so that the message can be displayed
+        ref.read(reconnectProvider.notifier).state = Duration.zero;
+        throw Exception('Failed after multiple retries: $e');
+      }
+    }
   }
 });
--- frontend/lib/ui/components/lobby_container.dart
+++ frontend/lib/ui/components/lobby_container.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:multipacman/clients/lobby_api.dart';
-import 'package:multipacman/gen/lobby/v1/lobby.pb.dart';
-import 'package:multipacman/providers.dart';
 import 'package:multipacman/ui/components/lobby_list.dart';
-import 'package:multipacman/ui/components/utils.dart';
-import 'package:multipacman/utils.dart';
 
 class LobbyGridView extends ConsumerWidget {
   const LobbyGridView({super.key});
@@ -13,25 +9,42 @@ class LobbyGridView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final lobbyList = ref.watch(lobbyListProvider);
+    final reconnectDelay = ref.watch(reconnectProvider);
 
-    return lobbyList.when(
-      data: (data) => LobbyList(data: data),
-      error: (error, stackTrace) => Center(
-        child: Column(
-          children: [
-            Text('Error fetching lobbies'),
-            Text(error.toString()),
-          ],
-        ),
-      ),
-      loading: () {
-        final val = ref.read(lobbyListProvider).valueOrNull;
-        if (val != null) {
-          return LobbyList(data: val);
-        }
+    return reconnectDelay != Duration.zero
+        ? Center(
+            child: Column(
+              children: [
+                Text(
+                  'Connection lost to server, attempting reconnection in ${reconnectDelay.inSeconds} seconds',
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () => ref.invalidate(lobbyListProvider),
+                  child: Text('Reconnect now'),
+                ),
+              ],
+            ),
+          )
+        : lobbyList.when(
+            data: (data) => LobbyList(data: data),
+            error: (error, stackTrace) => Center(
+              child: Column(
+                children: [
+                  Text('Error fetching lobbies'),
+                  Text(error.toString()),
+                ],
+              ),
+            ),
+            loading: () {
+              final val = ref.read(lobbyListProvider).valueOrNull;
+              if (val != null) {
+                return LobbyList(data: val);
+              }
 
-        return Center(child: CircularProgressIndicator());
-      },
-    );
+              return Center(child: CircularProgressIndicator());
+            },
+          );
   }
 }
